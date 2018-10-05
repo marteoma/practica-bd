861ab195144656b852852c65631a106be9b7b3af
Juanjoc12 <coche-9@hotmail.com>
2018-10-04 21:10:56 -0500

    Se agrega modelo Cassandra

--- masivo_insertar.php
+++ masivo_insertar.php
@@ -6,7 +6,7 @@
 */
 
 	/*Usted debe cambiar esto segun su configuracion del proyecto (ubicacion dentro del wampp y el puerto del pache*/
-	$URL_HOME = 'http://localhost:9090/practica-bd/';
+	$URL_HOME = 'http://localhost/practica-bd/';
 
 	/*Se recuperan los argumentos*/
 	$bd = htmlspecialchars($_GET["bd"]);
