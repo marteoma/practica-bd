b10156a4dc9e2d2e54095cb7148186ba267e9c47
marteoma <marteomasoyyo@gmail.com>
2018-10-13 17:35:55 -0500

    Scripts para llenar tablas maestras

---
+++ insert-lugares.sql
@@ -0,0 +1,10 @@
+INSERT INTO lugares (lugar) VALUES(0),
+			   (1),
+			   (2),
+			   (3),
+			   (4),
+			   (5),
+			   (6),
+			   (7),
+			   (8),
+			   (9)
---
+++ insert-placas.sql
@@ -0,0 +1,16 @@
+INSERT INTO vehiculos (placa) VALUES ('AAA111'),
+				     ('BBB111'),
+				     ('CCC111'),
+				     ('AAA222'),
+				     ('BBB222'),
+				     ('CCC222'),
+				     ('AAA333'),
+				     ('BBB333'),
+				     ('CCC333'),
+				     ('AAA444'),
+				     ('BBB444'),
+				     ('CCC444'),
+				     ('AAA555'),
+				     ('BBB555'),
+				     ('CCC555');
+				     
